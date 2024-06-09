fc087b42c6a72d4841aa85eb4267e6d4013b7e85
noumbissi16 <noumbissistael@gmail.com>
2024-06-09 14:20:40 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import * as AuthSession from "expo-auth-session";
+import * as Linking from "expo-linking";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Google from "expo-auth-session/providers/google";
+import { makeRedirectUri } from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import { useEffect, useState } from "react";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [userInfo, setUserInfo] = useState<any>(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    webClientId:
+      "29004774971-r161asskgn6jhicibgqt3jvp99o9vjjp.apps.googleusercontent.com",
+    // "29004774971-vtrv1kihaqaamhcjcqv7qgof5ccmg1md.apps.googleusercontent.com",
+    androidClientId:
+      "29004774971-dnuds44ufqd8brn658enfj6usphdgu79.apps.googleusercontent.com",
+    iosClientId:
+      "29004774971-afuqrs4itrhkbntk3q2grl06498n699o.apps.googleusercontent.com",
+    scopes: ["profile", "email"],
+  });
+
+  // AuthSession.useAuthRequest({clientId:"", redirectUri:""},{authorizationEndpoint:"dss"})
+
+  async function getUserInfo(token: any) {
+    // alert("dsds");
+    console.log("abs");
+
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const user = await response.json();
+      console.log(user);
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleLogin = async () => {
+    const user = await AsyncStorage.getItem("@user");
+    if (!user) {
+      if (response?.type === "success") {
+        await getUserInfo(response.authentication?.accessToken);
+      }
+    } else {
+      setUserInfo(JSON.parse(user));
+    }
+  };
+
+  useEffect(() => {
+    handleLogin();
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{JSON.stringify(userInfo, null, 2)}</Text>
+      {/* <Image source={{ uri: userInfo.picture }} width={50} height={50} /> */}
+
+      <Button title="Login with Google" onPress={() => promptAsync()} />
     </View>
   );
 }
@@ -13,8 +78,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
